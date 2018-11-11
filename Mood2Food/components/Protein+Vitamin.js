65447a0db2680a958c5fb662c09db277561d4ad4
Annie <lannie9400@gmail.com>
2018-11-10 23:56:36 -0500

    Create hyperlink when clicking the food name

---
+++ Mood2Food/components/Protein+Vitamin.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class Meat extends Component{
+
+  render(){
+    return (
+      <View style={styles.container}>
+       <Image source={{uri: "https://www.mycolombianrecipes.com/wp-content/uploads/2009/08/Salmon-con-Naranja.jpg", width: 100, height: 60}} />
+        <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/Orange-Salmon-2060197') } >Orange Salmon
+         </Text>
+          <Image source={{uri: "https://www.whats4eats.com/files/fish-fish-chips-flickr-10413717%40N08-Smabs-Sputzer-9355126330-4x3.jpg", width: 100, height: 60}} />
+          <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/English-Fish-_n-Chips-1005960') } >English Fish & Chips
+            </Text>
+            <Image source={{uri: "http://3.bp.blogspot.com/-lxkVRbvXsFU/UMjSH4Owq6I/AAAAAAAACPA/I59PNux4LfE/s1600/030+vitamin+vegetable+soup.jpgZ", width: 100, height: 60}} />   
+           <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/Vitamin-Soup-1445401') } >Vitamin Soup
+          </Text>
+         <Image source={{uri: "https://img-aws.ehowcdn.com/350x235p/photos.demandstudios.com/227/57/fotolia_9512288_XS.jpg", width: 100, height: 60}} />   
+           <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/Vitamin-C-Fruit-Smoothie-2546565') } >Vitamin Fruit Smoothie
+          </Text>
+      </View>
+    );
+  }
+};
+
+var styles = StyleSheet.create({
+  container:{
+     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  TextStyle: {
+    color: '#000080',
+    textDecorationLine: 'underline'
+  }
+});
---
+++ Mood2Food/components/Soft.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class Meat extends Component{
+
+  render(){
+    return (
+      <View style={styles.container}>
+       <Image source={{uri: "https://s-media-cache-ak0.pinimg.com/originals/f7/35/80/f73580b26ffb1ebea3872779b7b2842e.jpg", width: 130, height: 80}} />
+        <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/Soft-Homemade-Slider-Buns-2181607') } >Soft Slider Buns
+         </Text>
+          <Image source={{uri: "https://bakingamoment.com/wp-content/uploads/2018/08/IMG_9365-best-monster-cookies-square.jpg", width: 130, height: 80}} />
+          <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/Soft-Monster-Cookies-1801838') } >Soft Monster Cookies
+            </Text>
+            <Image source={{uri: "https://barefeetinthekitchen.com/wp-content/uploads/2015/12/crispy-cheese-bacon-potatoes-7.jpg", width: 130, height: 80}} />   
+       <Text style={styles.TextStyle} onPress={ ()=> Linking.openURL('https://www.yummly.com/recipe/Crispy-Cheese-and-Bacon-Potatoes-2245614') } >Cheese&Bacon Potatoes
+          </Text>
+      </View>
+    );
+  }
+};
+
+var styles = StyleSheet.create({
+  container:{
+     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  TextStyle: {
+    color: '#000080',
+    textDecorationLine: 'underline'
+  }
+});
